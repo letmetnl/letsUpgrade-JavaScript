65f2776f79ba95a9c5941e3a99f2eaf709c42db4
mohits Projects <mohit5150515@gmail.com>
2020-09-18 15:32:24 +0530

    day3-4 assignment added

---
+++ Day3-4/day3-4pro3.js
@@ -0,0 +1,32 @@
+let xobj = [
+  {
+    name: "Steve Smith",
+    age: 30,
+    country: "Aus",
+    hobbies: ["Leaving", "wierd shots", "winning"],
+  },
+  {
+    name: "Ms dhoni",
+    age: 38,
+    country: "Ind",
+    hobbies: ["smashing", "out of the park", "winning"],
+  },
+  {
+    " name": "Dale Steyn",
+    " age ": 36,
+    " country": "South africa",
+    hobbies: ["breaking toes", "breaking wickets", "winning"],
+  },
+  {
+    name: "Pooran",
+    age: 25,
+    " country": "WestIndies",
+    hobbies: ["Stumping", "shots", "makieng out"],
+  },
+];
+display();
+function display() {
+  for (let i = 0; i < xobj.length; i++) {
+    console.log(xobj[i]);
+  }
+}
